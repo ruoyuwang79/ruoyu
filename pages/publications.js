ada92ef853278510ba0560ca62f98c94262e374a
Ruoyu <aliceevaking@gmail.com>
2024-02-28 11:53:05 -0500

    update some symbols

--- pages/publications.js
+++ pages/publications.js
@@ -19,19 +19,19 @@ export default function Publications() {
         <div className={styles.publication}>
             <h2>2023</h2>
             <Link href="https://pubs.aip.org/asa/jasa/article/153/3_supplement/A345/2886474/Exploring-propagation-delay-and-mobility-in">
-                <p><b>Ruoyu Wang</b>, Yahong Rosa Zheng. &lquot;Exploring propagation delay and mobility in underwater acoustic networks&rquot;</p>
+                <p><b>Ruoyu Wang</b>, Yahong Rosa Zheng. &Prime;Exploring propagation delay and mobility in underwater acoustic networks&Prime;</p>
                 <p>In The Journal of the Acoustical Society of America 153 (3_supplement), A345-A345.</p>
             </Link>
             
             <h2>2022</h2>
             <Link href="https://dl.acm.org/doi/abs/10.1145/3567600.3569543">
-                <p>Xiyuan Zhu, Fengyi Sun, <b>Ruoyu Wang</b>, and Yahong Rosa Zheng. &lquot;Demo: Underwater Video Transmission Through Acoustic Communication Links&rquot;</p>
+                <p>Xiyuan Zhu, Fengyi Sun, <b>Ruoyu Wang</b>, and Yahong Rosa Zheng. &Prime;Demo: Underwater Video Transmission Through Acoustic Communication Links&Prime;</p>
                 <p>In WUWNet 2022.</p>
             </Link>
 
             <h2>2019</h2>
             <Link href="https://ieeexplore.ieee.org/abstract/document/8983491">
-                <p>Ma Yu, Dingcheng Jia, Huifan Zhang, <b>Ruoyu Wang</b>, and Pingqiang Zhou. &lquot;A Compact Memory Structure based on 2T1R Against Single-Event Upset in RRAM Arrays.&rquot;</p>
+                <p>Ma Yu, Dingcheng Jia, Huifan Zhang, <b>Ruoyu Wang</b>, and Pingqiang Zhou. &Prime;A Compact Memory Structure based on 2T1R Against Single-Event Upset in RRAM Arrays.&Prime;</p>
                 <p>In 2019 IEEE 13th International Conference on ASIC (ASICON), pp. 1-4. IEEE, 2019.</p>
             </Link>
         </div>
